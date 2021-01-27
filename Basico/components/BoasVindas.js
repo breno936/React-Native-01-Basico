5dd2fa2afeee92457fd91dd8079b8e836f839eec
breno936 <brenodelbem@gmail.com>
2021-01-27 18:15:43 +0000

    Alteracao(Aula 2)
    Signed-off-by: breno936 <brenodelbem@gmail.com>

--- Basico/components/BoasVindas.js
+++ Basico/components/BoasVindas.js
@@ -1,111 +1,108 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Alert, TouchableWithoutFeedback, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert} from 'react-native';
 
-function BoasVindas(props) {
-        const [sobrenome, setSobrenome] = useState('');
-        const [nome, setNome] = useState(setNome);
-        const [mensagem, setMensagem] = useState('Seja bem-vindo(a)!');
-        const [mostraMensagem, setMostraMensagem] = useState(false);
+function BoasVindas(props){
 
-  const _apresentaMensagem = () => {
-     if(!nome == '' && !sobrenome == ''){
-         setMostraMensagem(true);
-     } else {
-         alert(
-             'Prenchimento obrigatório',
-             'Informe o nome e o sobrenome!',
-             [
-                 {text:'OK'},
-             ],
-         );
-     }
-  }
+    const[nome, setNome] = useState('');
+    const[mensagem, setMensagem] = useState('Seja bem-vindo(a)!');
+    const[sobrenome, setSobrenome] = useState('');
+    const[mostraMensagem, setMostraMensagem] = useState(false);
 
-  const _limpar = () =>{
-      setSobrenome('');
-      setNome('');
-      setMostraMensagem(false);
-  }
-  return (
-    <View style={styles.container}>
-        <TextInput
-        style={styles.campo}
-        placeholder="Digite seu nome"
-        onChangeText ={nome => setNome(nome)}
-        value={nome}
+    const _apresentarMensagem = () => {
+        if(!nome == '' && !sobrenome == ''){
+            setMostraMensagem(true)
+        }else{
+            Alert.alert(
+                'Preenchimento Obrigatório',
+                'Informe o nome e sobrenome!',
+                [
+                    {text: 'OK'},
+                ],
+            );
+        }
+    }
 
-        />
+    const _limpar = () =>{
+        setNome('');
+        setSobrenome('');
+        setMostraMensagem('');
+    }
+    return(
+        <View style={Styles.container}>
+<Text style={styles.titulo}>Identifique-se</Text>
 
+          <TextInput
+            style={styles.campo}
+            placeholder="digite seu nome"
+            onChangeText={nome => setNome(nome)}
+            value={nome}
+            />
 
-        <TextInput
-        style={styles.campo}
-        placeholder="Digite seu nome"
-        onChangeText ={sobrenome => setSobrenome(sobrenome)}
-        value={sobrenome}
+            
+          <TextInput
+            style={styles.campo}
+            placeholder="digite seu Sobrenome"
+            onChangeText={nome => setSobreNome(nome)}
+            value={sobrenome}
+            />
 
-        />
+            <View style={styles.botaoContainer}>
+                <Button
+                onPress = {_apresentarMensagem}
+                title = 'Ok'/>
+            </View>
 
-        {mostraMensagem && (
-        <View>
-      <Text style={styles.texto}>Olá,{nome} {sobrenome}! {props.saudacao}</Text>
-      <Text style={styles.texto}>{mensagem}</Text>
+            {mostraMensagem &&(
+                <View>
+                    <Text style={styles.texto}>Olá, {nome} {sobrenome}! {props.saudacao}</Text>
+                    <Text style={styles.texto}>{mensagem}</Text>
+                </View>
+          )}
+
+          <View style={styles.botaoContainer}>
+              <Button
+              onPress={_limpar}
+              title="Limpar"/>
+          </View>
+
+            <Text style={styles.texto}>Ola {props.saudacao}</Text>
+            <Text style={styles.texto}>Ola {mensagem}</Text>
         </View>
 
-    )}
-
-        <View style={styles.botaoContainer}>
-            <Button 
-            onPress={_limpar}
-            title='Limpar'/>
-        </View>
-
-
-        <View style={styles.botaoContainer}>
-            <Button 
-            onPress={_apresentaMensagem}
-
-            title='ok'/>
-        </View>
-
-    
-    
-     
-
-    </View>
-
-    
-  )
+    )
 }
 const styles = StyleSheet.create({
-  container: {
-     backgroundColor: '#081a31',
-     width: 300,
-     borderRadius: 5,
-     padding: 10,
-     margin: 10
-  },
-  texto: {
-    fontSize: 14,
-    color: '#FFFFFF',
-    textAlign: "center",
-  },
-  campo:{
-    backgroundColor:'#dcedff',
-    borderRadius: 5,
-    marginBottom:5,
-    marginTop:5,
-    height:35,
-    fontSize:14,
-
-    
-  },
-
-  botaoContainer:{
-        marginTop:10,
-        marginBottom:10,
-        borderRadius:10,
+    container:{
+        backgroundColor : '#081a31',
+        width: 300,
+        borderRadius: 5,
+        padding: 10,
+        margin: 10
     },
-
+    texto:{
+        fontSize : 14,
+        color: '#FFFFFF',
+        textAlign: 'center'
+    },
+    campo:{
+    backgroundColor: '#dcedff',
+    fontSize: 14,
+    marginBottom: 5,
+    marginTop: 5,
+    borderRadius: 5,
+    height: 35    
+    },
+    titulo:{
+        fontSize: 16,
+        color: '#FFFFFFF',
+        fontWeight: 'bold',
+        textAlign: "center",
+    },
+    botaoContainer:{
+        marginBottom: 5,
+        marginTop: 5,
+        borderRadius: 5,
+    },
 });
 
 export default BoasVindas;
